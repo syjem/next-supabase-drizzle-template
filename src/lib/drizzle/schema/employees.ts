6b071cc2af294ac8770b8a9001a957f2a0f7807a
Jemuel Repoylo <syjem143@gmail.com>
2026-03-26 19:23:35 +0800

    fix: remove active column

--- src/lib/drizzle/schema/employees.ts
+++ src/lib/drizzle/schema/employees.ts
@@ -1,11 +1,10 @@
-import { boolean, numeric, pgPolicy, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { numeric, pgPolicy, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const employees = pgTable('employees', {
     id: uuid().primaryKey().defaultRandom(),
     created_at: timestamp({withTimezone: true}).defaultNow(),
     name: text(),
     employee_id: numeric().unique(),
-    active: boolean().default(true),
 }, () => [
     pgPolicy("Public Access", {
         as: 'permissive',
