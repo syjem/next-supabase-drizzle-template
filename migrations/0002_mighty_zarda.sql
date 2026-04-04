44bf9c9fb45450ec74d02b1cf67dd924f852343d
Jemuel Repoylo <syjem143@gmail.com>
2026-04-04 15:34:06 +0800

    feat: migration for employees table with not null constraints

---
+++ migrations/0002_mighty_zarda.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "employees" ALTER COLUMN "created_at" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "employees" ALTER COLUMN "name" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "employees" ALTER COLUMN "employee_id" SET NOT NULL;
