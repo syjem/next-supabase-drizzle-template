a2e37121be92f8ebf8da266c718fe2ec43535b01
Jemuel Repoylo <syjem143@gmail.com>
2026-04-04 15:36:04 +0800

    refactor: enforce not null constraints in employees schema

--- src/lib/drizzle/schema/employees.ts
+++ src/lib/drizzle/schema/employees.ts
@@ -1,17 +1,28 @@
-import { numeric, pgPolicy, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import {
+  numeric,
+  pgPolicy,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from 'drizzle-orm/pg-core';
 
-export const employees = pgTable('employees', {
+export const employees = pgTable(
+  'employees',
+  {
     id: uuid().primaryKey().defaultRandom(),
-    created_at: timestamp({withTimezone: true}).defaultNow(),
-    name: text(),
-    employee_id: numeric().unique(),
-}, () => [
-    pgPolicy("Public Access", {
-        as: 'permissive',
-        for: "select",
-        to: "public",
-    })
-]);
+    created_at: timestamp({ withTimezone: true }).notNull().defaultNow(),
+    name: text().notNull(),
+    employee_id: numeric().unique().notNull(),
+  },
+  () => [
+    pgPolicy('Public Access', {
+      as: 'permissive',
+      for: 'select',
+      to: 'public',
+    }),
+  ],
+);
 
 export type Employee = typeof employees.$inferSelect;
 export type NewEmployee = typeof employees.$inferInsert;
