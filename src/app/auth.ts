765b342866edeef8dd1903ab5835812ebe6a32e1
Jemuel Repoylo <syjem143@gmail.com>
2026-05-27 23:38:09 +0800

    refactor: rename getOptionalUser to getUser and add comments for clarity

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,7 +1,8 @@
 import { createClient } from '#/lib/supabase/server';
 import { redirect } from 'next/navigation';
 
-export async function getOptionalUser() {
+// Use this to a route where user is optional, for example a home page
+export async function getUser() {
   const supabase = await createClient();
   const { data, error } = await supabase.auth.getClaims();
 
@@ -12,8 +13,9 @@ export async function getOptionalUser() {
   return data.claims;
 }
 
+// Use this to a route where user is required, for example a dashboard page
 export async function getUserForProtectedRoutes() {
-  const user = await getOptionalUser();
+  const user = await getUser();
 
   if (!user) {
     redirect('/auth/sign-in');
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,10 @@
-import { getOptionalUser } from '#/app/auth';
+import { getUser } from '#/app/auth';
 import { Button } from '#/components/ui/button';
 import { UserAvatar } from '#/components/user-avatar';
 import Link from 'next/link';
 
 export async function Header() {
-  const user = await getOptionalUser();
+  const user = await getUser();
 
   return (
     <nav className="w-full max-w-7xl mx-auto px-4 py-6 sm:px-6 lg:px-8 flex items-center justify-between border-b">
